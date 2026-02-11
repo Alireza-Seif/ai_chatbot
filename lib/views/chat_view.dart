95573e088e2b94ea66b05a2bc2f207291b3d079d
alireza <alirezaariye@gmail.com>
2026-02-12 01:14:58 +0330

    feat(chat): integrate GetX ChatViewModel and add message input composer

--- lib/views/chat_view.dart
+++ lib/views/chat_view.dart
@@ -1,12 +1,49 @@
+import 'package:ai_chatbot/view_models/chat_viewmodel.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class ChatView extends StatelessWidget {
-  const ChatView({super.key});
+  final ChatViewModel chatViewModel = Get.put(ChatViewModel());
+  ChatView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('chat view'),),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(title: Text('chat view')),
+        body: Column(children: [Spacer(), _buildInputMessage()]),
+      ),
+    );
+  }
+
+  Widget _buildInputMessage() {
+    final TextEditingController textEditingController = TextEditingController();
+
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 8),
+      color: Colors.white,
+      child: Row(
+        children: [
+          Expanded(
+            child: TextField(
+              controller: textEditingController,
+              decoration: InputDecoration(
+                hintText: 'Enter Your Message',
+                border: InputBorder.none,
+              ),
+            ),
+          ),
+          IconButton(
+            onPressed: () {
+              chatViewModel.addMessage(
+                message: textEditingController.text,
+                isFromUser: true,
+              );
+            },
+            icon: Icon(Icons.send),
+          ),
+        ],
+      ),
     );
   }
 }
