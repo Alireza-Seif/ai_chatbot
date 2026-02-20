34bfe5c7a1926311323a864b3c501872c2312ae0
alireza <alirezaariye@gmail.com>
2026-02-20 23:59:52 +0330

    feat: wire ChatViewModel to ApiService

--- lib/view_models/chat_viewmodel.dart
+++ lib/view_models/chat_viewmodel.dart
@@ -1,10 +1,29 @@
 import 'package:ai_chatbot/models/message_model.dart';
+import 'package:ai_chatbot/services/api_service.dart';
 import 'package:get/get.dart';
 
 class ChatViewModel extends GetxController {
   var messages = <MessageModel>[].obs;
+  final ApiService _apiService = ApiService();
 
-  void addMessage({required String message, required bool isFromUser}){
+  void addMessage({required String message, required bool isFromUser}) {
     messages.add(MessageModel(text: message, isFromUser: isFromUser));
+  }
+
+  Future<void> sendMessageToAi(String message) async {
+    addMessage(message: message, isFromUser: true);
+
+    try {
+      final response = await _apiService.sendMessage(
+        message: message,
+        conversation: messages.toList(),
+      );
+
+      final reply = response['response'] as String;
+
+      addMessage(message: reply, isFromUser: false);
+    } catch (ex) {
+      addMessage(message: "Error: ${ex.toString()}", isFromUser: false);
+    }
   }
 }
